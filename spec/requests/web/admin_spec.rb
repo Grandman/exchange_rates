18bef3533012036755544de8fb34eafcb6e5545a
Grandman <root@grandman73.ru>
2016-03-29 00:30:16 +0400

    test for wrong params; test main if exist forced rate

--- spec/requests/web/admin_spec.rb
+++ spec/requests/web/admin_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'admin', type: :request do
   let!(:forced_rate) { create :forced_rate }
-  let(:forced_rate_attributes) { attributes_for :forced_rate}
+  let(:forced_rate_attributes) { attributes_for :forced_rate }
+  let(:wrong_forced_rate_attributes) { attributes_for :forced_rate, dollar_rate: -1 }
   before{ WebMock.allow_net_connect! }
 
   context 'index' do
@@ -17,6 +18,12 @@ RSpec.describe 'admin', type: :request do
       post '/admin', forced_rate_form: forced_rate_attributes
       expect(Rate.forced).to be_exists(dollar_rate: forced_rate_attributes[:dollar_rate])
       expect(response).to be_redirect
+    end
+
+    it 'not create model if wrong params' do
+      post '/admin', forced_rate_form: wrong_forced_rate_attributes
+      expect(Rate.forced).not_to be_exists(dollar_rate: forced_rate_attributes[:dollar_rate])
+      expect(response).to be_success
     end
   end
 end
--- spec/requests/web/main_spec.rb
+++ spec/requests/web/main_spec.rb
@@ -2,7 +2,15 @@ require 'rails_helper'
 
 RSpec.describe 'main', type: :request do
   context 'index' do
+    let(:forced_rate) { create :forced_rate, date_to: 5.day.from_now  }
+
     it 'render with 200 status' do
+      get "/"
+      expect(response).to be_success
+    end
+
+    it 'render with 200 status if exists forced rate' do
+      forced_rate
       get "/"
       expect(response).to be_success
     end
