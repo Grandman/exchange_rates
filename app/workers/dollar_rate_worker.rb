af3fb2ed93de1a0f6a8aa3bcc9c573aba120fb9a
Grandman <root@grandman73.ru>
2016-03-28 23:18:03 +0400

    dont run parser if isset forced rate

--- app/workers/dollar_rate_worker.rb
+++ app/workers/dollar_rate_worker.rb
@@ -4,11 +4,11 @@ class DollarRateWorker
   mattr_accessor(:parser) { RbcDollarRateParser }
 
   def perform
+    last_forced = Rate.forced.last
+    return if last_forced.present? && last_forced.date_to > DateTime.current
     rate = parser.perform
     return if rate.nil?
     Rate.create!(dollar_rate: rate)
-    last_forced = Rate.forced.last
-    return if last_forced.present? && last_forced.date_to > DateTime.current
     PublishService.perform(rate)
   end
 end
